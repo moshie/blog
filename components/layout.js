57223e4c8ca9f51c9628b89c3a43afbdc31a2e51
David Hewitt <hello@wp-unite.io>
2018-04-02 19:38:00 +0100

    initial commit

---
+++ components/layout.js
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default ({ children, title = 'Blog · David Hewitt' }) => (
+    <div>
+        <Head>
+            <meta charSet='utf-8' />
+            <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+            <title>{ title }</title>
+        </Head>
+        <header>
+            <nav>
+                <Link href='/'><a>Home</a></Link>
+            </nav>
+        </header>
+
+        <main>
+            { children }
+        </main>
+
+        <footer>
+
+        </footer>
+  </div>
+)
